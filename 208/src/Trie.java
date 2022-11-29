7fa6cc7a84b27c885b3f0201722a3d44440e1dc5
roo00 <34959987+zhouruicn@users.noreply.github.com>
2022-11-29 13:31:47 +0800

    Add files via upload

---
+++ 208/src/Trie.java
@@ -0,0 +1,74 @@
+import java.util.HashMap;
+
+public class Trie {
+
+    private Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+
+    public void insert(String word) {
+        Node node = root;
+        Node temp;
+        char[] chars = word.toCharArray();
+        for (Character ch : chars) {
+            temp = node.map.get(ch);
+            if (null == temp) {
+                temp = new Node();
+                node.map.put(ch, temp);
+            }
+            node = temp;
+        }
+        node.wordEnd = true;
+    }
+
+    public boolean search(String word) {
+        Node node = root;
+        char[] chars = word.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            node = node.map.get(chars[i]);
+            if (null == node) {
+                return false;
+            }
+        }
+        return ((null != node) && node.wordEnd);
+    }
+
+    public boolean startsWith(String prefix) {
+        Node node = root;
+        char[] chars = prefix.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            node = node.map.get(chars[i]);
+            if (null == node) {
+                return false;
+            }
+        }
+        return (null != node);
+    }
+
+    private class Node {
+
+        private boolean wordEnd = false;
+
+        private HashMap<Character, Node> map = new HashMap<>();
+
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        // trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.startsWith("app"));
+        System.out.println(trie.startsWith("appq"));
+    }
+
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
