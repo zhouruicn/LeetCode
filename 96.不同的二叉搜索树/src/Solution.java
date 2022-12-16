d8439d90092b43cf7b6a634a5df536f7489c1b87
zhourui <zhouuri@o2oa.net>
2022-12-16 15:40:49 +0800

    update

--- 96.不同的二叉搜索树/src/Solution.java
+++ 96.不同的二叉搜索树/src/Solution.java
@@ -1,5 +1,24 @@
 class Solution {
     public int numTrees(int n) {
-
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = 0;
+            for (int j = 1; j < i; j++) {
+                dp[i] = dp[i] + (dp[j - 1] * dp[i - j]);
+            }
+        }
+        return dp[n];
     }
 }
